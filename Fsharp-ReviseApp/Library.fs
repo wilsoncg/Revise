4efd2772c21391c09d645622a4df5ac0209a3354
Craig Wilson <craig@wilsoncg.net>
2019-02-14 21:04:33 +0000

    revise fibonacci

---
+++ Fsharp-ReviseApp/Library.fs
@@ -0,0 +1,7 @@
+﻿namespace Fsharp_ReviseApp
+
+module Library =
+
+ let fibs = 
+  (0, 1) |> 
+  Seq.unfold(fun (first, second) -> Some(first, (second, first + second)))
---
+++ ReviseApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Fsharp_ReviseApp;
+
+namespace ReviseApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var f1 = fib().Take(10);
+            Console.WriteLine(string.Join(",", f1));
+            Console.WriteLine();
+            var f2 = Library.fibs.Take(10);
+            Console.WriteLine(string.Join(",", f2));
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> fib()
+        {
+            var first = 0;
+            var second = 1;
+
+            yield return first;
+            yield return second;
+
+            while (true)
+            {
+                (first, second) = (second, first + second);
+                yield return second;
+            }
+        }
+    }
+}
