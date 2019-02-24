01a32c566846e5a4daf7716e5088578c61d4e2d8
Craig Wilson <craig@wilsoncg.net>
2019-02-24 10:11:15 +0000

    not quite

--- ReviseApp/Revise3.cs
+++ ReviseApp/Revise3.cs
@@ -8,31 +8,68 @@ namespace ReviseApp
 {
     public class Revise3
     {
+        private IList<int> hull(Point2D[] A)
+        {
+            var s = toPointWithIndex(A).Take(3).ToList();
+            var notInConvexHull = new List<int>();
+
+            for (int i = 3; i < A.Count(); i++)
+            {
+                while (
+                    s.Count() > 0 &&
+                    AngleBetween3Points(
+                        s[s.Count() - 2].Point,
+                        s[s.Count() - 1].Point,
+                        A[i]) < 180)
+                {
+                    notInConvexHull.Add(s[s.Count() - 1].index);
+                    s.RemoveAt(s.Count() - 1);                    
+                }
+                s.Add(new PointWithIndex { Point = A[i], index = i });
+            }
+
+            return notInConvexHull;
+        }
+
+        IList<PointWithIndex> toPointWithIndex(Point2D[] points)
+        {
+            var list = new List<PointWithIndex>();
+            points.Aggregate(
+                0,
+                (s, x) =>
+                {
+                    list.Add(new PointWithIndex { Point = x, index = s });
+                    return s + 1;
+                });
+            return list;
+        }
+
         // https://app.codility.com/programmers/lessons/99-future_training/polygon_concavity_index/
         public int PolygonConcavitiyIndex(Point2D[] A)
         {
-            var ordered = A.OrderByDescending(p => p.x);
-            var pivot = ordered.First();
-
-            var seed = new ConcavityAccum {
-                NotInConvexHull = new List<int>(),
-                Point = pivot };
-
-            var r =
-                ordered
-                .Skip(1)
-                .Aggregate(
-                    seed,
-                    (a, p) => 
-                    {
-                        //if(Math. a.Point)
-                        return a;
-                    },
-                    (a) => a.NotInConvexHull);
-            if (!r.Any())
+            if (A.Count() < 4)
                 return -1;
 
-            return r.First();
+            var notInHull = hull(A);
+            if (!notInHull.Any())
+                return -1;
+
+            return notInHull.First();
+        }
+
+        public double LeftOnOrRight(int x1, int y1, int x2, int y2, int x3, int y3)
+        {
+            return LeftOnOrRight(
+                new Point2D { x = x1, y = y1 },
+                new Point2D { x = x2, y = y2 },
+                new Point2D { x = x3, y = y3 });
+        }
+
+        public double LeftOnOrRight(Point2D p0, Point2D p1, Point2D p2)
+        {
+            return 
+                ((p1.x - p0.x) * (p2.y - p0.y)) - 
+                ((p2.x - p0.x) * (p1.y - p0.y));
         }
 
         public double AngleBetween2Vectors(
@@ -43,13 +80,24 @@ namespace ReviseApp
         }
 
         public double AngleBetween3Points(
+            Point2D a, Point2D b, Point2D c)
+        {
+            var r = AngleBetween3Points(a.x, a.y, b.x, b.y, c.x, c.y);
+            Console.WriteLine($"Angle is {r} for {a.x},{a.y} {b.x},{b.y} {c.x},{c.y}");
+            return r;
+        }
+
+        public double AngleBetween3Points(
             int x1, int y1, int x2, int y2, int x3, int y3)
         {
-            return 
-                Math.Abs(
-                    Angle(
-                        new Vector2(y3 - y1, x3 - x1),
-                        new Vector2(y2 - y1, x2 - x1)));
+            Vector2 v1 = new Vector2(x2 - x1, y2 - y1);
+            Vector2 v2 = new Vector2(x3 - x2, y3 - y2);
+            return Math.Abs(Angle(v1, v2));
+        }
+
+        double radiansToDegrees(double r)
+        {
+            return r * (180 / Math.PI);
         }
 
         double Angle(
@@ -57,7 +105,12 @@ namespace ReviseApp
             Vector2 v2)
         {
             var radians = Math.Atan2(v2.Y, v2.X) - Math.Atan2(v1.Y, v1.X);
-            return radians * (180 / Math.PI);
+            if (radians == 0)
+                return radiansToDegrees(Math.PI);
+            if (radians < 0)
+                return radiansToDegrees(radians + (2 * Math.PI)); 
+
+            return radiansToDegrees(radians);
         }
 
         class ConcavityAccum
@@ -76,6 +129,7 @@ namespace ReviseApp
     }
 
     public class Point2D { public int x; public int y; }
+    public class PointWithIndex { public Point2D Point; public int index; }
 
     [TestClass]
     public class Revise3Tests
@@ -92,9 +146,31 @@ namespace ReviseApp
         }
 
         [TestMethod]
+        public void NotInHullReturnsEither2or6()
+        {
+            var input = new[] { (-1, 3), (1, 2), (1, 1), (3, 1), (0, -1), (-2, 1), (-1, 2) };
+
+            var r3 = new Revise3();
+            var result = r3.PolygonConcavitiyIndex(r3.ToPoints(input));
+
+            Assert.IsTrue(result == 2 | result == 6);
+        }
+
+        [TestMethod]
+        public void LastPointNotInHull()
+        {
+            var input = new[] { (-1, 3), (1, 2), (3, 1), (0, -1), (-2, 1), (-1, 2) };
+
+            var r3 = new Revise3();
+            var result = r3.PolygonConcavitiyIndex(r3.ToPoints(input));
+
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
         public void SquareShouldBeMinus1()
         {
-            var input = new[] { (1, 3), (1, 1), (3, 1), (3, 3) };
+            var input = new[] { (1, 3), (3, 3), (3, 1), (1, 1) };
 
             var r3 = new Revise3();
             var result = r3.PolygonConcavitiyIndex(r3.ToPoints(input));
@@ -103,12 +179,59 @@ namespace ReviseApp
         }
 
         [TestMethod]
-        public void Angle1()
+        public void CollinearTest()
         {
-            var r1 = new Revise3().AngleBetween2Vectors(2, 0, 0, 1);
-            var r2 = new Revise3().AngleBetween3Points(1, 1, 1, 2, 3, 1);
-            Assert.AreEqual(90, r1);
-            Assert.AreEqual(r1, r2);
+            var r1 = new Revise3().LeftOnOrRight(-1, 3, 1, 2, 3, 1);
+            Assert.AreEqual(0, r1);
+        }
+
+        [TestMethod]
+        public void TurnLeft()
+        {
+            var r2 = new Revise3().LeftOnOrRight(1, 2, 1, 1, 3, 1);
+            Assert.IsTrue(r2 > 0, $"was {r2}");
+        }
+
+        [TestMethod]
+        public void TurnRight()
+        {
+            var r1 = new Revise3().LeftOnOrRight(1, 2, 3, 1, 0, -1);
+            Assert.IsTrue(r1 < 0, $"was {r1}");
+        }
+
+        [TestMethod]
+        public void TurnRightAlso()
+        {
+            var r1 = new Revise3().LeftOnOrRight(3, 1, 0, -1, -2, 1);
+            Assert.IsTrue(r1 < 0, $"was {r1}");
+        }
+
+        [TestMethod]
+        public void AngleCollinearTest()
+        {
+            var r1 = new Revise3().AngleBetween3Points(-1, 3, 1, 2, 3, 1);
+            Assert.AreEqual(180, r1);
+        }
+
+        [TestMethod]
+        public void Angle90()
+        {
+            var r2 = new Revise3().AngleBetween3Points(1, 2, 1, 1, 3, 1);
+            Assert.IsTrue(r2 == 90, $"was {r2}");
+        }
+
+        [TestMethod]
+        public void AngleGreaterThan90()
+        {
+            var r1 = new Revise3().AngleBetween3Points(1, 2, 3, 1, 0, -1);
+            Assert.IsTrue(r1 > 90, $"was {r1}");
+        }
+
+        [TestMethod]
+        public void AnotherAngleGreaterThan90()
+        {
+            var r1 = new Revise3().AngleBetween3Points(3, 1, 0, -1, -2, 1);
+            Assert.IsTrue(r1 > 90, $"was {r1}");
         }
     }
 }
