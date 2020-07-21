cab1a03990dd4762b0e476bb5ebeac10575d41d6
Craig Wilson <craig@wilsoncg.net>
2020-07-21 22:16:39 +0100

    possible custom logger

--- CoyotePlayground/CoyoteAsyncQueue.cs
+++ CoyotePlayground/CoyoteAsyncQueue.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks.Dataflow;
+using Microsoft.Coyote;
 using Microsoft.Coyote.Random;
 using Microsoft.Coyote.Runtime;
 using Microsoft.Coyote.Specifications;
 using Microsoft.Coyote.Tasks;
+using static CoyotePlayground.InterestingEvent;
 
 namespace CoyotePlayground
 {
@@ -33,6 +36,43 @@ namespace CoyotePlayground
         }
     }
 
+    public class InterestingEvent 
+    {
+        public DateTime DateTime;
+        public string TaskName;
+        public StateEnum State;
+
+        public enum StateEnum { 
+            SemaphoreWait,
+            SemaphoreEnter,
+            SemaphoreReleased,
+            MutexAttemptAcquire,
+            MutexAcquired,
+        }
+    }
+
+    public class InterestingEvents
+    {
+        List<InterestingEvent> interestingEvents = new List<InterestingEvent>();
+
+        public void Mark(string taskName, StateEnum state)
+        {
+            interestingEvents.Add(
+                new InterestingEvent
+                {
+                    DateTime = DateTime.UtcNow,
+                    TaskName = taskName,
+                    State = state
+                });
+        }
+
+        public string SerializeToJson()
+        {
+            var json = JsonSerializer.Serialize(this.interestingEvents);
+            return json;
+        }
+    }
+
     /// <summary>
     /// Warning: Produces deadlock!
     /// </summary>
@@ -43,9 +83,9 @@ namespace CoyotePlayground
         AsyncLock _mutex;
         Semaphore _consumerSemaphore;
         Semaphore _producerSemaphore;
-        TextWriter _logger;
+        InterestingEvents _logger;
 
-        public CoyoteAsyncQueue(int capacity, TextWriter logger)
+        public CoyoteAsyncQueue(int capacity, InterestingEvents logger)
         {
             _logger = logger;
             _buffer = new Queue<T>();
@@ -56,37 +96,39 @@ namespace CoyotePlayground
 
         public async Task Enqueue(T item, string taskName)
         {
-            _logger.WriteLine($"{taskName} Producer about to enter semaphore count: {_producerSemaphore.CurrentCount}");
+            _logger.Mark(taskName, StateEnum.SemaphoreWait);
             await _producerSemaphore.WaitAsync();
-            _logger.WriteLine($"{taskName} Producer entered semaphore");
-            
+            _logger.Mark(taskName, StateEnum.SemaphoreEnter);
+
+            _logger.Mark(taskName, StateEnum.MutexAttemptAcquire);
             using (await _mutex.AcquireAsync())
             {
+                _logger.Mark(taskName, StateEnum.MutexAcquired);
                 _buffer.Enqueue(item);
             }
 
-            // wakeup consumers
-            _logger.WriteLine($"{taskName} Consumer about to release semaphore count: {_consumerSemaphore.CurrentCount}");
+            // wakeup consumers            
             _consumerSemaphore.Release();
-            _logger.WriteLine($"{taskName} Consumer released semaphore count: {_consumerSemaphore.CurrentCount}");
+            _logger.Mark(taskName, StateEnum.SemaphoreReleased);
         }
 
         public async Task<T> Dequeue(string taskName)
         {
             T item;
-            _logger.WriteLine($"{taskName} Consumer about to release semaphore count: {_consumerSemaphore.CurrentCount}");
+            _logger.Mark(taskName, StateEnum.SemaphoreWait);
             await _consumerSemaphore.WaitAsync();
-            _logger.WriteLine($"{taskName} Consumer entered semaphore");
+            _logger.Mark(taskName, StateEnum.SemaphoreEnter);
 
+            _logger.Mark(taskName, StateEnum.MutexAttemptAcquire);
             using (await _mutex.AcquireAsync())
             {
+                _logger.Mark(taskName, StateEnum.MutexAcquired);
                 item = _buffer.Dequeue();
             }
 
-            // wakeup producers
-            _logger.WriteLine($"{taskName} Producer about to release semaphore count: {_producerSemaphore.CurrentCount}");
+            // wakeup producers            
             _producerSemaphore.Release();
-            _logger.WriteLine($"{taskName} Producer released semaphore count: {_producerSemaphore.CurrentCount}");
+            _logger.Mark(taskName, StateEnum.SemaphoreReleased);
 
             return item;
         }
@@ -156,7 +198,7 @@ namespace CoyotePlayground
             var numProducers = numConsumerProducer;
 
             log($"Testing Queue with {numConsumerProducer} consumers and producers");
-            var queue = new CoyoteAsyncQueue<int>(numConsumerProducer, runtime.Logger);
+            var queue = new CoyoteAsyncQueue<int>(numConsumerProducer, new InterestingEvents());
 
             var tasks =
                 Chop(RandomStream(generator, 100), numProducers)
@@ -178,7 +220,7 @@ namespace CoyotePlayground
 
             var numConsumerProducer = 2;
             log($"Testing Queue with {numConsumerProducer} consumers and producers");
-            var queue = new CoyoteAsyncQueue<int>(numConsumerProducer + 1, runtime.Logger);
+            var queue = new CoyoteAsyncQueue<int>(numConsumerProducer + 1, new InterestingEvents());
 
             var tasks = new List<Task>()
             {                
