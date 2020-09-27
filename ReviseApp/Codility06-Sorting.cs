77ac871be0b465ea6ddbc5497ba1962837a2d88c
Craig Wilson <craig@wilsoncg.net>
2020-09-27 16:50:31 +0100

    intersections couting

--- ReviseApp/Codility06-Sorting.cs
+++ ReviseApp/Codility06-Sorting.cs
@@ -9,6 +9,57 @@ namespace ReviseApp
 {
     public class Codility06_Sorting
     {
+        public class IntersectionAccumulator
+        {
+            public int numIntersections;
+            public Tuple<int, int> Disc;
+        }
+
+        IEnumerable<Tuple<T, int>> WithIndex<T>(IEnumerable<T> list)
+        {
+            var e = list.GetEnumerator();
+            int i = 0;
+            while (e.MoveNext())
+            {
+                yield return Tuple.Create(e.Current, i);
+                i++;
+            }
+        }
+
+        public int NumberOfIntersections(int[] A)
+        {            
+            if (A.Length <= 1)
+                return 0;
+
+            var sortedByRadiusDescending = 
+                WithIndex(A)
+                .OrderByDescending(a => a.Item1);
+
+            var first = sortedByRadiusDescending.First();
+            var xs = sortedByRadiusDescending.Skip(1);
+
+            var r = 
+                xs.Aggregate(
+                    new IntersectionAccumulator { Disc = first },
+                    (j, k) => {
+                        var rightEdgeOfj = j.Disc.Item2 + j.Disc.Item1;
+                        var leftEdgeOfk = k.Item2 - k.Item1;
+                        var rightEdgeOfk = k.Item2 + k.Item1;
+
+                        // check rightEdge of j crosses with leftEdge of k
+                        if (leftEdgeOfk <= rightEdgeOfj || 
+                            rightEdgeOfj >= rightEdgeOfk)
+                            j.numIntersections++;
+
+                        j.Disc = k;
+                        return j;
+                    });
+
+            if (r.numIntersections > 10000000)
+                return -1;
+            return r.numIntersections;
+        }
+        
         public int MaxProductOfThree(int[] A)
         {
             var sorted = A.OrderByDescending(a => a).ToArray();
@@ -77,7 +128,52 @@ namespace ReviseApp
             var input = new[] { -5, 4, -5, 5 };
             var r1 = sorting.MaxProductOfThree(input);
 
-            Assert.AreEqual(-5*-5*5, r1);
+            Assert.AreEqual(-5 * -5 * 5, r1);
+        }
+
+        [TestMethod]
+        public void Intersections_ForSample()
+        {
+            var input = new[] { 1, 5, 2, 1, 4, 0 };
+            var r = sorting.NumberOfIntersections(input);
+
+            Assert.AreEqual(11, r);
+        }
+
+        [TestMethod]
+        public void Intersections_ForOnePoint_AndTwoDiscs()
+        {
+            var input = new[] { 0, 0, 1 };
+            var r = sorting.NumberOfIntersections(input);
+
+            Assert.AreEqual(1, r);
+        }
+
+        [TestMethod]
+        public void Intersections_ForTwoDiscs()
+        {
+            var input = new[] { 2, 1 };
+            var r = sorting.NumberOfIntersections(input);
+
+            Assert.AreEqual(1, r);
+        }
+
+        [TestMethod]
+        public void Intersections_ForTwoDiscs_AndOnePoint()
+        {
+            var input = new[] { 2, 1, 0 };
+            var r = sorting.NumberOfIntersections(input);
+
+            Assert.AreEqual(2, r);
+        }
+
+        [TestMethod]
+        public void Intersections_ShouldBeThree()
+        {
+            var input = new[] { 1, 5, 2 };
+            var r = sorting.NumberOfIntersections(input);
+
+            Assert.AreEqual(3, r);
         }
     }
 }
